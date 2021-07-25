2b107a5b4131450476829446e3373de564e0afc5
Arfaz <aferoz@hotmail.fr>
2021-07-25 18:39:19 +0800

    Create application properties and dependencies

---
+++ src/main/java/com/crowdcube/CrowdcubeApplication.java
@@ -0,0 +1,11 @@
+package com.crowdcube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdcubeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdcubeApplication.class);
+    }
+}
